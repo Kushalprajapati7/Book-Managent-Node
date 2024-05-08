e2028a450c84c7227fc1e9098a8395794020d7f6
Kushal Prajapati <kushalbprajapati2810@gmail.com>
2024-05-08 12:57:47 +0530

    Adde few lines of code for autor

---
+++ src/controllers/User.Controller.ts
@@ -0,0 +1,105 @@
+import { inject, injectable } from "inversify";
+import { Request, Response } from "express";
+import { UserService } from "../services/User.Service";
+import { interfaces, controller, httpGet, httpPost, httpDelete, request, queryParam, response, requestParam } from "inversify-express-utils";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import verifyToken from '../middleware/userMiddleware'
+
+
+@controller("/users")
+export class userController {
+    constructor(@inject('UserService') private userService: UserService) { }
+
+    @httpPost('/signup')
+    async createUser(req: Request, res: Response): Promise<void> {
+        try {
+
+            const { username, password,email,role } = req.body;
+            await this.userService.createUser(username, password,email,role);
+            res.status(200).send("User Created Successfully");
+        }
+        catch (error) {
+            console.log('Error while signUp');
+            res.status(500).json({ message: error })
+
+        }
+    }
+
+    @httpPost("/login")
+    async loginUser(req: Request, res: Response): Promise<void> {
+        try {
+            const { username, password } = req.body;
+            const user: any = await this.userService.getUserByUsername(username);
+
+            if (!user) {
+                res.status(401).json({ error: 'Authentication failed' });
+                return
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password)
+
+            if (!passwordMatch) {
+                res.status(401).json({ error: 'Authentication failed' });
+                return
+            }
+            const token = jwt.sign({ userId: user._id }, 'KP', { expiresIn: '1h' })
+            if (user.role === 'author') {
+                res.status(403).json({ error: 'Forbidden' }); // Forbid authors from using this endpoint
+                return;
+            }
+            res.status(200).json({ token })
+        }
+        catch (error) {
+            console.error("Error logging in");
+            res.status(500).json({ message: error })
+        }
+    }
+
+    @httpPost("/authorLogin")
+    async authorLogin(req: Request, res: Response): Promise<void> {
+        try {
+            const { username, password } = req.body;
+            const author: any = await this.userService.getUserByUsername(username);
+
+            if (!author) {
+                res.status(401).json({ error: 'Authentication failed' });
+                return;
+            }
+
+            const passwordMatch = await bcrypt.compare(password, author.password);
+
+            if (!passwordMatch) {
+                res.status(401).json({ error: 'Authentication failed' });
+                return;
+            }
+
+            if (author.role !== 'author') {
+                res.status(403).json({ error: 'Forbidden' });
+                return;
+            }
+
+            const token = jwt.sign({ userId: author._id }, 'KP', { expiresIn: '1h' });
+            res.status(200).json({ token });
+        }
+        catch (error) {
+            console.error("Error author login");
+            res.status(500).json({ message: error });
+        }
+    }
+
+
+    @httpGet('/',verifyToken)
+    async getAllUsers(req: Request, res: Response): Promise<void> {
+        try {
+            const users = await this.userService.getAllUsers();
+            res.status(200).json(users);
+        } catch (error) {
+            console.error("Error retrieving users:", error);
+            res.status(500).send('Failed to retrieve users');
+        }
+    }
+
+}
+
+
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -12,10 +12,6 @@ import { bookController } from "./controllers/Book.Controller";
 
 
 const container = new Container();
-<<<<<<< HEAD
-
-=======
->>>>>>> a0752a0774e74b328403341a3f59f7ac3b365a7e
 container.bind<userController>("userController").to(userController)
 container.bind<UserService>("UserService").to(UserService)
 
